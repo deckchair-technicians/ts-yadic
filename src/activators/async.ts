8f16926c39114cb10972b133a565c613145bb8de
Matt Savage <matthew.savage@gmail.com>
2018-11-12 13:10:55 +0000

    wait(), to help with asynchronous activators

---
+++ src/activators/async.ts
@@ -0,0 +1,20 @@
+import {pluck} from "../util/magic";
+
+export type Resolve<T> = T extends Promise<infer U> ? U : T;
+export type Resolved<T> = { [K in keyof T]: Resolve<T[K]> }
+
+export async function resolve<T>(t: T): Promise<Resolved<T>> {
+  const result = <Resolved<T>>{};
+  for (const k of Object.keys(t)) {
+    result[k] = await t[k];
+  }
+  return result;
+}
+
+export function wait<T, K extends keyof T, V>(keys: K[], fn: (c: Resolved<Pick<T, K>>) => V): (container: T) => Promise<V> {
+  return async (container: T) : Promise<V> => {
+    const plucked = pluck(container, keys);
+    const resolved = await resolve(plucked);
+    return fn(resolved);
+  }
+}
---
+++ test/activators/async.test.ts
@@ -0,0 +1,60 @@
+import {Activators, rollup} from "../../src/activators";
+import {lazy} from "../../src/containers";
+import {expect} from "chai";
+import {wait} from "../../src/activators/async";
+
+describe("asynchronous activators", () => {
+    it("Activators that return promises work as you'd expect", async () => {
+      type A = {
+        a: Promise<string>;
+      }
+      type B = {
+        b: Promise<string>;
+      }
+      // a depends on nothing
+      const aActivators: Activators<A> = {
+        a: async (_: A) => {
+          return "value a"
+        }
+      };
+
+      // b depends on a
+      const bActivators: Activators<B, A> = {
+        b: async (container: A) => {
+          return "b saw: " + await container.a
+        }
+      };
+
+      const dependencies: A & B = lazy(rollup(aActivators, bActivators));
+
+      expect(await dependencies.a).eq("value a");
+      expect(await dependencies.b).eq("b saw: value a");
+    });
+
+    it("wait() can help simplify constructors that take a few dependencies", async () => {
+      type A = {
+        a: Promise<string>;
+        b: Promise<string>;
+      }
+      type B = {
+        c: Promise<string>;
+      }
+      // a depends on nothing
+      const aActivators: Activators<A> = {
+        a: async (_: A) => "value a",
+        b: async (_: A) => "value b"
+      };
+
+      // b depends on a
+      const bActivators: Activators<B, A> = {
+        c: wait(["a", "b"], (c) => `c saw: ${c.a}, ${c.b}`)
+      };
+
+      const dependencies: A & B = lazy(rollup(aActivators, bActivators));
+
+      expect(await dependencies.a).eq("value a");
+      expect(await dependencies.b).eq("value b");
+      expect(await dependencies.c).eq("c saw: value a, value b");
+    });
+  }
+);
