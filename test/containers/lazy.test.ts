ede8972926f833594db5ea1a0a6f3eb8a9495136
Matt Savage <matthew.savage@gmail.com>
2019-02-12 17:43:09 +0000

    Minor: typo

--- test/containers/lazy.test.ts
+++ test/containers/lazy.test.ts
@@ -1,7 +1,7 @@
 import {expect} from "chai";
 import {lazy} from "../../index";
 
-describe("constainers,lazy", () => {
+describe("containers.lazy()", () => {
   it("Returns values from activator", () => {
     type Thing = {
       a: string;
